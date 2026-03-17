ffaa4f484e054bac80844e9ccf3648e9ac7718fc
Aditya Thakur <adityathakur6176@gmail.com>
2026-03-17 19:56:46 +0545

    fixed notesnepal to NotesNepal

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  base: "/notesnepal/",
+  base: "/NotesNepal/",
   plugins: [react()],
   server: {
     port: 5173
